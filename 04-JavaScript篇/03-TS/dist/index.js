3d8f2ae32dd3e12386a138482ff54386854366dc
wpcc <310624463@qq.com>
2022-05-10 18:18:43 +0800

    add typescript

--- 04-JavaScript篇/03-TS/dist/index.js
+++ 04-JavaScript篇/03-TS/dist/index.js
@@ -1,5 +1,76 @@
 "use strict";
-let id = 5;
-let company = 'Traversy Media';
-let ids = [1, 2, 3, 4];
-console.log('ID:', id);
+// basic type
+let str;
+let num;
+let bool;
+let x;
+let un; // undefined
+str = 'hello world';
+num = 123;
+un = 333;
+un = `hello world`;
+bool = true;
+x = 'some';
+// array
+let ids;
+ids = [1, 2, 3];
+// Tuple
+let person;
+let persons;
+// enum
+var direction1;
+(function (direction1) {
+    direction1[direction1["Up"] = 0] = "Up";
+    direction1[direction1["Down"] = 1] = "Down";
+    direction1[direction1["Left"] = 2] = "Left";
+    direction1[direction1["Right"] = 3] = "Right";
+})(direction1 || (direction1 = {}));
+console.log('enum', direction1.Up);
+var direction2;
+(function (direction2) {
+    direction2[direction2["Up"] = 1] = "Up";
+    direction2[direction2["Down"] = 2] = "Down";
+    direction2[direction2["Left"] = 3] = "Left";
+    direction2[direction2["Right"] = 4] = "Right";
+})(direction2 || (direction2 = {}));
+console.log('eunm2', direction2.Up);
+var direction3;
+(function (direction3) {
+    direction3["Up"] = "Up";
+    direction3["Down"] = "Down";
+    direction3["Left"] = "Left";
+    direction3["Right"] = "Right";
+})(direction3 || (direction3 = {}));
+console.log('enum3', direction3.Up);
+// Object
+const user1 = {
+    id: 123,
+    name: 'John',
+};
+const user2 = {
+    id: 12,
+    name: 'sala',
+};
+// Type Assertion
+let cid = 1;
+// let customerId = <number>cid
+let customerId = cid;
+// Function
+function addNum(x, y) {
+    return x + y;
+}
+// Void
+function log(message) {
+    console.log(message);
+}
+const user3 = {
+    name: 'Hayes',
+    id: 0,
+};
+class UserAccount {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+const user4 = new UserAccount('nurphy', 1);
--- 04-JavaScript篇/03-TS/src/index.ts
+++ 04-JavaScript篇/03-TS/src/index.ts
@@ -1,10 +1,104 @@
-function say(): void {
-  console.log("hello world")
+// basic type
+let str: string
+let num: Number
+let bool: Boolean
+let x: any
+let un // undefined
+
+str = 'hello world'
+num = 123
+un = 333
+un = `hello world`
+bool = true
+x = 'some'
+
+// array
+let ids: number[]
+
+ids = [1, 2, 3]
+
+// Tuple
+let person: [number, string, boolean]
+let persons: [number][]
+
+// enum
+enum direction1 {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+console.log('enum', direction1.Up)
+
+enum direction2 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+console.log('eunm2', direction2.Up)
+
+enum direction3 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right',
 }
 
-say()
-;(function () {
-  console.log("hello")
-})()
+console.log('enum3', direction3.Up)
 
-let a: string
+// Object
+const user1: {
+  id: number
+  name: string
+} = {
+  id: 123,
+  name: 'John',
+}
+
+type TUser = {
+  id: number
+  name: string
+}
+
+const user2: TUser = {
+  id: 12,
+  name: 'sala',
+}
+
+// Type Assertion
+let cid: any = 1
+// let customerId = <number>cid
+let customerId = cid as number
+
+// Function
+function addNum(x: number, y: number): number {
+  return x + y
+}
+// Void
+function log(message: string | number): void {
+  console.log(message)
+}
+
+// interface
+interface User {
+  name: String
+  id: Number
+}
+
+const user3: User = {
+  name: 'Hayes',
+  id: 0,
+}
+
+class UserAccount {
+  name: string
+  id: number
+
+  constructor(name: string, id: number) {
+    this.name = name
+    this.id = id
+  }
+}
+
+const user4: User = new UserAccount('nurphy', 1)
